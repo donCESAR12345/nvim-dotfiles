f8ee064dfcd191adadbf80f5373cd9864294ed7c
donCESAR12345 <sprit152009@gmail.com>
2024-01-02 14:24:54 -0500

    [Feature] Basic IDE functionality with minor additions
    Signed-off-by: donCESAR12345 <sprit152009@gmail.com>

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,19 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        hijack_netrw_behavior = "open_default"
+      }
+    })
+
+		vim.keymap.set("n", "<leader>e", ":Neotree<CR>", {})
+		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+	end,
+}
